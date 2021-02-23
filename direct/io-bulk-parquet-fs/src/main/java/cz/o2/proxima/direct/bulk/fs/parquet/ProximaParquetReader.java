81aedcee46c9a19b72c581156347239cee270ada
Lukas Drbal <lukas.drbal@gmail.com>
2021-02-23 22:56:52 +0100

    Introduce parquet file format for bulk filesystem storages

---
+++ direct/io-bulk-parquet-fs/src/main/java/cz/o2/proxima/direct/bulk/fs/parquet/ProximaParquetReader.java
@@ -0,0 +1,328 @@
+/**
+ * Copyright 2017-2021 O2 Czech Republic, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.o2.proxima.direct.bulk.fs.parquet;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.AbstractIterator;
+import cz.o2.proxima.direct.bulk.Path;
+import cz.o2.proxima.direct.bulk.Reader;
+import cz.o2.proxima.direct.bulk.fs.parquet.ParquetFileFormat.OPERATION;
+import cz.o2.proxima.repository.AttributeDescriptor;
+import cz.o2.proxima.repository.EntityDescriptor;
+import cz.o2.proxima.storage.StreamElement;
+import cz.o2.proxima.util.ExceptionUtils;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.SeekableByteChannel;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.api.InitContext;
+import org.apache.parquet.hadoop.api.ReadSupport;
+import org.apache.parquet.io.DelegatingSeekableInputStream;
+import org.apache.parquet.io.InputFile;
+import org.apache.parquet.io.SeekableInputStream;
+import org.apache.parquet.io.api.Binary;
+import org.apache.parquet.io.api.Converter;
+import org.apache.parquet.io.api.GroupConverter;
+import org.apache.parquet.io.api.PrimitiveConverter;
+import org.apache.parquet.io.api.RecordMaterializer;
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.LogicalTypeAnnotation;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.PrimitiveType;
+import org.apache.parquet.schema.TypeConverter;
+
+@Slf4j
+public class ProximaParquetReader implements Reader {
+
+  private final Path path;
+  private final ParquetReader<StreamElement> reader;
+
+  public ProximaParquetReader(Path path, EntityDescriptor entity, String attributeNamesPrefix)
+      throws IOException {
+    final SeekableByteChannel channel = (SeekableByteChannel) path.read();
+    final Configuration configuration = new Configuration(false);
+    this.reader =
+        new ParquetReadBuilder(new BulkInputFile(channel), entity, attributeNamesPrefix)
+            .withConf(configuration)
+            /**
+             * Currently we can not use push down filter for attributes See
+             * https://github.com/O2-Czech-Republic/proxima-platform/issues/196 for details
+             * .withFilter()
+             */
+            .build();
+    this.path = path;
+  }
+
+  @Override
+  public void close() {
+    ExceptionUtils.unchecked(reader::close);
+  }
+
+  @Override
+  public Path getPath() {
+    return path;
+  }
+
+  @Override
+  public Iterator<StreamElement> iterator() {
+    return new AbstractIterator<StreamElement>() {
+      @Override
+      protected StreamElement computeNext() {
+        try {
+          StreamElement element = reader.read();
+          if (element == null) {
+            return endOfData();
+          } else {
+            return element;
+          }
+        } catch (IOException e) {
+          throw new IllegalStateException("Unable to compute next element.", e);
+        }
+      }
+    };
+  }
+
+  private static class ParquetReadBuilder extends ParquetReader.Builder<StreamElement> {
+
+    private final EntityDescriptor entity;
+    private final String attributeNamesPrefix;
+
+    ParquetReadBuilder(InputFile file, EntityDescriptor entity, String attributeNamesPrefix) {
+      super(file);
+      this.entity = entity;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+    }
+
+    @Override
+    protected ReadSupport<StreamElement> getReadSupport() {
+      Preconditions.checkNotNull(entity, "Entity must be specified.");
+      return new StreamElementReadSupport(entity, attributeNamesPrefix);
+    }
+  }
+
+  private static class StreamElementReadSupport extends ReadSupport<StreamElement> {
+
+    private final EntityDescriptor entity;
+    private final String attributeNamesPrefix;
+
+    public StreamElementReadSupport(EntityDescriptor entity, String attributeNamesPrefix) {
+      this.entity = entity;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+    }
+
+    @Override
+    public ReadContext init(InitContext context) {
+      return new ReadContext(context.getFileSchema());
+    }
+
+    @Override
+    public RecordMaterializer<StreamElement> prepareForRead(
+        Configuration configuration,
+        Map<String, String> keyValueMetaData,
+        MessageType fileSchema,
+        ReadContext readContext) {
+      return new StreamElementRecordMaterializer(fileSchema, entity, attributeNamesPrefix);
+    }
+  }
+
+  private static class StreamElementRecordMaterializer extends RecordMaterializer<StreamElement> {
+
+    private final GroupConverter root;
+    private final EntityDescriptor entity;
+    private final String attributeNamesPrefix;
+    private Map<String, Object> record = new HashMap<>();
+
+    public StreamElementRecordMaterializer(
+        MessageType schema, EntityDescriptor entity, String attributeNamesPrefix) {
+      this.entity = entity;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+      this.root =
+          (GroupConverter)
+              schema.convertWith(
+                  new TypeConverter<Converter>() {
+                    @Override
+                    public Converter convertPrimitiveType(
+                        List<GroupType> path, PrimitiveType primitiveType) {
+                      return new PrimitiveConverter() {
+                        @Override
+                        public void addBinary(Binary value) {
+                          if (primitiveType.getLogicalTypeAnnotation() != null) {
+                            if (primitiveType
+                                .getLogicalTypeAnnotation()
+                                .equals(LogicalTypeAnnotation.stringType())) {
+                              record.put(primitiveType.getName(), value.toStringUsingUTF8());
+                            } else if (primitiveType
+                                .getLogicalTypeAnnotation()
+                                .equals(LogicalTypeAnnotation.enumType())) {
+                              record.put(primitiveType.getName(), new String(value.getBytes()));
+                            } else {
+                              record.put(primitiveType.getName(), value.getBytes());
+                            }
+                          } else {
+                            record.put(primitiveType.getName(), value.getBytes());
+                          }
+                        }
+
+                        @Override
+                        public void addBoolean(boolean value) {
+                          record.put(primitiveType.getName(), value);
+                        }
+
+                        @Override
+                        public void addDouble(double value) {
+                          record.put(primitiveType.getName(), value);
+                        }
+
+                        @Override
+                        public void addFloat(float value) {
+                          record.put(primitiveType.getName(), value);
+                        }
+
+                        @Override
+                        public void addInt(int value) {
+                          record.put(primitiveType.getName(), value);
+                        }
+
+                        @Override
+                        public void addLong(long value) {
+                          record.put(primitiveType.getName(), value);
+                        }
+                      };
+                    }
+
+                    @Override
+                    public Converter convertGroupType(
+                        List<GroupType> path, GroupType groupType, List<Converter> converters) {
+                      log.info("convertGroupType called {} {}", path, converters);
+                      return new GroupConverter() {
+
+                        public Converter getConverter(int fieldIndex) {
+                          return converters.get(fieldIndex);
+                        }
+
+                        public void start() {
+                          // current = "start()";
+                        }
+
+                        public void end() {
+                          // current = "end()";
+                        }
+                      };
+                    }
+
+                    @Override
+                    public Converter convertMessageType(
+                        MessageType messageType, List<Converter> children) {
+                      log.info("convertMessageType called {} {} ", messageType, children);
+                      return convertGroupType(null, messageType, children);
+                    }
+                  });
+    }
+
+    @Override
+    public StreamElement getCurrentRecord() {
+      final String key =
+          (String) getRequiredValueFromCurrentRowData(ParquetFileFormat.PARQUET_COLUMN_NAME_KEY);
+      final String operation =
+          (String)
+              getRequiredValueFromCurrentRowData(ParquetFileFormat.PARQUET_COLUMN_NAME_OPERATION);
+      final String attributeName =
+          (String)
+              getRequiredValueFromCurrentRowData(ParquetFileFormat.PARQUET_COLUMN_NAME_ATTRIBUTE);
+      Optional<AttributeDescriptor<Object>> attribute = entity.findAttribute(attributeName);
+      if (!attribute.isPresent()) {
+        // current attribute is not in entity -> skip
+        return null;
+      }
+      final String uuid =
+          (String) getRequiredValueFromCurrentRowData(ParquetFileFormat.PARQUET_COLUMN_NAME_UUID);
+      final long timestamp =
+          (long)
+              getRequiredValueFromCurrentRowData(ParquetFileFormat.PARQUET_COLUMN_NAME_TIMESTAMP);
+      switch (OPERATION.of(operation)) {
+        case DELETE:
+          return StreamElement.delete(entity, attribute.get(), uuid, key, attributeName, timestamp);
+        case DELETE_WILDCARD:
+          return StreamElement.deleteWildcard(entity, attribute.get(), uuid, key, timestamp);
+        case UPSERT:
+          return StreamElement.upsert(
+              entity,
+              attribute.get(),
+              uuid,
+              key,
+              attributeName,
+              timestamp,
+              getValueFromCurrentRowData(attribute.get()));
+        default:
+          throw new RecordMaterializationException("Unknown operation " + operation);
+      }
+    }
+
+    private byte[] getValueFromCurrentRowData(AttributeDescriptor<?> attribute) {
+      // @FIXME
+      return (byte[]) record.get(attributeNamesPrefix + attribute.toAttributePrefix(false));
+    }
+
+    private Object getRequiredValueFromCurrentRowData(String column) {
+      return Optional.ofNullable(record.getOrDefault(column, null))
+          .orElseThrow(
+              () ->
+                  new IllegalStateException("Unable to read required value for column " + column));
+    }
+
+    @Override
+    public GroupConverter getRootConverter() {
+      return this.root;
+    }
+  }
+
+  private static class BulkInputFile implements InputFile {
+
+    private final SeekableByteChannel channel;
+
+    BulkInputFile(SeekableByteChannel channel) {
+      this.channel = channel;
+    }
+
+    @Override
+    public long getLength() throws IOException {
+      return channel.size();
+    }
+
+    @Override
+    public SeekableInputStream newStream() {
+      return new DelegatingSeekableInputStream(Channels.newInputStream(channel)) {
+
+        @Override
+        public long getPos() throws IOException {
+          return channel.position();
+        }
+
+        @Override
+        public void seek(long newPosition) throws IOException {
+          channel.position(newPosition);
+        }
+      };
+    }
+  }
+}
---
+++ direct/io-bulk-parquet-fs/src/main/java/cz/o2/proxima/direct/bulk/fs/parquet/ProximaParquetWriter.java
@@ -0,0 +1,330 @@
+/**
+ * Copyright 2017-2021 O2 Czech Republic, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.o2.proxima.direct.bulk.fs.parquet;
+
+import cz.o2.proxima.direct.bulk.Path;
+import cz.o2.proxima.direct.bulk.Writer;
+import cz.o2.proxima.direct.bulk.fs.parquet.ParquetFileFormat.OPERATION;
+import cz.o2.proxima.scheme.AttributeValueType;
+import cz.o2.proxima.scheme.SchemaDescriptors.SchemaTypeDescriptor;
+import cz.o2.proxima.storage.StreamElement;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.parquet.hadoop.ParquetFileWriter.Mode;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.api.WriteSupport;
+import org.apache.parquet.io.InvalidRecordException;
+import org.apache.parquet.io.OutputFile;
+import org.apache.parquet.io.PositionOutputStream;
+import org.apache.parquet.io.api.Binary;
+import org.apache.parquet.io.api.RecordConsumer;
+import org.apache.parquet.schema.MessageType;
+
+@Slf4j
+public class ProximaParquetWriter implements Writer {
+
+  private final Path path;
+  private final ParquetWriter<StreamElement> writer;
+
+  public ProximaParquetWriter(
+      Path path, MessageType schema, String attributeNamesPrefix, Configuration configuration)
+      throws IOException {
+    this.path = path;
+    this.writer =
+        new ParquetWriterBuilder(new BulkOutputFile(path.writer()), schema, attributeNamesPrefix)
+            .withConf(configuration)
+            .withWriteMode(Mode.OVERWRITE)
+            .build();
+  }
+
+  @Override
+  public void write(StreamElement elem) throws IOException {
+    try {
+      writer.write(elem);
+    } catch (InvalidRecordException iex) {
+      throw new IllegalArgumentException("Unable to write StreamElement.", iex);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    writer.close();
+  }
+
+  @Override
+  public Path getPath() {
+    return path;
+  }
+
+  private static class ParquetWriterBuilder
+      extends ParquetWriter.Builder<StreamElement, ParquetWriterBuilder> {
+
+    private final MessageType parquetSchema;
+    private final String attributeNamesPrefix;
+
+    private ParquetWriterBuilder(
+        OutputFile outputFile, MessageType parquetSchema, String attributeNamesPrefix) {
+      super(outputFile);
+      this.parquetSchema = parquetSchema;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+    }
+
+    @Override
+    protected ParquetWriterBuilder self() {
+      return this;
+    }
+
+    @Override
+    protected WriteSupport<StreamElement> getWriteSupport(Configuration conf) {
+      return new StreamElementWriteSupport(parquetSchema, attributeNamesPrefix);
+    }
+  }
+
+  private static class StreamElementWriteSupport extends WriteSupport<StreamElement> {
+
+    private final MessageType parquetSchema;
+    private final String attributeNamesPrefix;
+    private StreamElementParquetWriter writer;
+
+    public StreamElementWriteSupport(MessageType parquetSchema, String attributeNamesPrefix) {
+      this.parquetSchema = parquetSchema;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+    }
+
+    @Override
+    public WriteContext init(Configuration configuration) {
+      return new WriteContext(parquetSchema, Collections.emptyMap());
+    }
+
+    @Override
+    public void prepareForWrite(RecordConsumer recordConsumer) {
+      this.writer =
+          new StreamElementParquetWriter(recordConsumer, parquetSchema, attributeNamesPrefix);
+    }
+
+    @Override
+    public void write(StreamElement record) {
+      writer.write(record);
+    }
+  }
+
+  private static class StreamElementParquetWriter {
+
+    private final RecordConsumer recordConsumer;
+    private final MessageType parquetSchema;
+    private final String attributeNamesPrefix;
+    private Map<String, SchemaTypeDescriptor<?>> schemasCache = new HashMap<>();
+
+    public StreamElementParquetWriter(
+        RecordConsumer recordConsumer, MessageType parquetSchema, String attributeNamesPrefix) {
+      this.recordConsumer = recordConsumer;
+      this.parquetSchema = parquetSchema;
+      this.attributeNamesPrefix = attributeNamesPrefix;
+    }
+
+    public void write(StreamElement element) {
+      recordConsumer.startMessage();
+      writeStreamElementHeader(element);
+      if (element.getValue() != null && element.getValue().length > 0) {
+        writeAttribute(element);
+      }
+      recordConsumer.endMessage();
+    }
+
+    private void writeAttribute(StreamElement element) {
+      log.trace("Writing stream element {}", element);
+      String attribute =
+          attributeNamesPrefix + element.getAttributeDescriptor().toAttributePrefix(false);
+      SchemaTypeDescriptor<?> attributeSchema =
+          schemasCache.computeIfAbsent(
+              attribute,
+              name ->
+                  element.getAttributeDescriptor().getValueSerializer().getValueSchemaDescriptor());
+      log.debug("Writing attribute {}", attribute);
+      writeValue(attribute, attributeSchema, element.getValue());
+    }
+
+    private void writeValue(String name ,SchemaTypeDescriptor<?> schema, byte[] value) {
+      writeStartField(name);
+      switch (schema.getType()) {
+        case STRUCTURE:
+          recordConsumer.startGroup();
+          schema
+              .getStructureTypeDescriptor()
+              .getFields()
+              .forEach(
+                  (field, type) -> {
+                    log.info("Trying to write field {} with type {}", field, type);
+                  });
+          // FIXME
+          recordConsumer.endGroup();
+          break;
+        case BYTE:
+        case STRING:
+        case ENUM:
+          recordConsumer.addBinary(Binary.fromReusedByteArray(value));
+          break;
+        case ARRAY:
+          if (schema
+              .getArrayTypeDescriptor()
+              .getValueDescriptor()
+              .getType()
+              .equals(AttributeValueType.BYTE)) {
+            // Array of bytes should be encoded just as binary
+            recordConsumer.addBinary(Binary.fromReusedByteArray(value));
+          } else {
+            recordConsumer.startGroup();
+            // FIXME
+            recordConsumer.endGroup();
+          }
+          break;
+        case LONG:
+          long longVal = Long.parseLong(new String(value));
+          recordConsumer.addLong(longVal);
+          break;
+        case INT:
+          int intVal = Integer.parseInt(new String(value));
+          recordConsumer.addInteger(intVal);
+          break;
+        case DOUBLE:
+          double doubleVal = Double.parseDouble(new String(value));
+          recordConsumer.addDouble(doubleVal);
+          break;
+        case FLOAT:
+          float floatVal = Float.parseFloat(new String(value));
+          recordConsumer.addFloat(floatVal);
+          break;
+        case BOOLEAN:
+          boolean boolVal = Boolean.parseBoolean(new String(value));
+          recordConsumer.addBoolean(boolVal);
+          break;
+      }
+      writeEndField(name);
+    }
+
+    private void writeStartField(String name) {
+      recordConsumer.startField(name, parquetSchema.getFieldIndex(name));
+    }
+
+    private void writeEndField(String name) {
+      recordConsumer.endField(name, parquetSchema.getFieldIndex(name));
+    }
+
+    private void writeStreamElementHeader(StreamElement element) {
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_KEY);
+      recordConsumer.addBinary(Binary.fromString(element.getKey()));
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_KEY);
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_UUID);
+      recordConsumer.addBinary(Binary.fromString(element.getUuid()));
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_UUID);
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_TIMESTAMP);
+      recordConsumer.addLong(element.getStamp());
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_TIMESTAMP);
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_OPERATION);
+      if (element.isDeleteWildcard()) {
+        recordConsumer.addBinary(Binary.fromString(OPERATION.DELETE_WILDCARD.getValue()));
+      } else if (element.isDelete()) {
+        recordConsumer.addBinary(Binary.fromString(OPERATION.DELETE.getValue()));
+      } else {
+        recordConsumer.addBinary(Binary.fromString(OPERATION.UPSERT.getValue()));
+      }
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_OPERATION);
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_ATTRIBUTE);
+      recordConsumer.addBinary(Binary.fromString(element.getAttribute()));
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_ATTRIBUTE);
+      writeStartField(ParquetFileFormat.PARQUET_COLUMN_NAME_ATTRIBUTE_PREFIX);
+      recordConsumer.addBinary(
+          Binary.fromString(element.getAttributeDescriptor().toAttributePrefix()));
+      writeEndField(ParquetFileFormat.PARQUET_COLUMN_NAME_ATTRIBUTE_PREFIX);
+    }
+  }
+
+  private static class BulkOutputFile implements OutputFile {
+
+    private final OutputStream outputStream;
+
+    BulkOutputFile(OutputStream outputStream) {
+      this.outputStream = outputStream;
+    }
+
+    @Override
+    public PositionOutputStream create(long blockSizeHint) {
+      return new BulkOutputStream(outputStream);
+    }
+
+    @Override
+    public PositionOutputStream createOrOverwrite(long blockSizeHint) {
+      return new BulkOutputStream(outputStream);
+    }
+
+    @Override
+    public boolean supportsBlockSize() {
+      return false;
+    }
+
+    @Override
+    public long defaultBlockSize() {
+      return 0;
+    }
+  }
+
+  private static class BulkOutputStream extends PositionOutputStream {
+
+    private final OutputStream delegate;
+    private long position = 0;
+
+    private BulkOutputStream(OutputStream delegate) {
+      this.delegate = delegate;
+    }
+
+    @Override
+    public long getPos() {
+      return position;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      position++;
+      delegate.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+      write(b, 0, b.length);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+      delegate.write(b, off, len);
+      position += len;
+    }
+
+    @Override
+    public void flush() throws IOException {
+      delegate.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+      delegate.close();
+    }
+  }
+}
