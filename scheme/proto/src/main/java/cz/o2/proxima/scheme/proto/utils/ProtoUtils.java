152cec46a1c79fda1628d5b77de6ac16761a54a2
Lukas Drbal <lukas.drbal@gmail.com>
2021-02-26 00:08:45 +0100

    WIP: introduce attribute value accessors

--- scheme/proto/src/main/java/cz/o2/proxima/scheme/proto/utils/ProtoUtils.java
+++ scheme/proto/src/main/java/cz/o2/proxima/scheme/proto/utils/ProtoUtils.java
@@ -15,34 +15,61 @@
  */
 package cz.o2.proxima.scheme.proto.utils;
 
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.EnumValueDescriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Message;
+import com.google.protobuf.Message.Builder;
+import cz.o2.proxima.scheme.AttributeValueAccessors.ArrayValueAccessor;
+import cz.o2.proxima.scheme.AttributeValueAccessors.EnumValueAccessor;
+import cz.o2.proxima.scheme.AttributeValueAccessors.PrimitiveValueAccessor;
 import cz.o2.proxima.scheme.SchemaDescriptors;
 import cz.o2.proxima.scheme.SchemaDescriptors.SchemaTypeDescriptor;
 import cz.o2.proxima.scheme.SchemaDescriptors.StructureTypeDescriptor;
+import cz.o2.proxima.scheme.SchemaDescriptors.TypeDescriptor;
+import cz.o2.proxima.scheme.proto.ProtoMessageValueAccessor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ProtoUtils {
 
-  private static Map<String, SchemaTypeDescriptor<?>> structCache = new ConcurrentHashMap<>();
+  private static final Map<String, SchemaTypeDescriptor<?>> structCache = new ConcurrentHashMap<>();
 
   private ProtoUtils() {
     // no-op
   }
 
-  public static <T> SchemaTypeDescriptor<T> convertProtoToSchema(Descriptor proto) {
-    StructureTypeDescriptor<T> schema = SchemaDescriptors.structures(proto.getName());
-    proto.getFields().forEach(f -> schema.addField(f.getName(), convertField(f)));
+  public static <T extends Message> SchemaTypeDescriptor<T> convertProtoToSchema(
+      Descriptor proto, T defaultValue) {
+    final Map<String, TypeDescriptor<?>> fields =
+        proto
+            .getFields()
+            .stream()
+            .collect(
+                Collectors.toMap(
+                    FieldDescriptor::getName,
+                    f -> convertField(f, defaultValue.newBuilderForType())));
+
+    StructureTypeDescriptor<T> schema =
+        SchemaDescriptors.structures(
+            proto.getName(),
+            Collections.emptyMap(),
+            new ProtoMessageValueAccessor<>(fields, () -> proto, () -> defaultValue));
+    fields.forEach(schema::addField);
     return schema.toTypeDescriptor();
   }
 
   @SuppressWarnings("unchecked")
-  protected static <T> SchemaTypeDescriptor<T> convertField(FieldDescriptor proto) {
+  protected static <T> SchemaTypeDescriptor<T> convertField(
+      FieldDescriptor proto, Builder builder) {
     SchemaTypeDescriptor<T> descriptor;
 
     switch (proto.getJavaType()) {
@@ -53,7 +80,31 @@ public class ProtoUtils {
         descriptor = (SchemaTypeDescriptor<T>) SchemaDescriptors.booleans().toTypeDescriptor();
         break;
       case BYTE_STRING:
-        descriptor = (SchemaTypeDescriptor<T>) SchemaDescriptors.bytes().toTypeDescriptor();
+        descriptor =
+            (SchemaTypeDescriptor<T>)
+                SchemaDescriptors.bytes(
+                        new PrimitiveValueAccessor<byte[]>() {
+                          @Override
+                          public byte[] createFrom(Object object) {
+                            return ((ByteString) object).toByteArray();
+                          }
+
+                          @Override
+                          public Object valueOf(byte[] value) {
+                            return ByteString.copyFrom(value);
+                          }
+
+                          @Override
+                          public byte[] asBytes(Object object) {
+                            return ((ByteString) object).toByteArray();
+                          }
+
+                          @Override
+                          public Object fromBytes(byte[] bytes) {
+                            return ByteString.copyFrom(bytes);
+                          }
+                        })
+                    .toTypeDescriptor();
         break;
       case FLOAT:
         descriptor = (SchemaTypeDescriptor<T>) SchemaDescriptors.floats().toTypeDescriptor();
@@ -68,21 +119,62 @@ public class ProtoUtils {
         descriptor = (SchemaTypeDescriptor<T>) SchemaDescriptors.integers().toTypeDescriptor();
         break;
       case ENUM:
+        final Map<String, EnumValueDescriptor> enumValues =
+            proto
+                .getEnumType()
+                .getValues()
+                .stream()
+                .collect(Collectors.toMap(EnumValueDescriptor::getName, Function.identity()));
+
         descriptor =
             (SchemaTypeDescriptor<T>)
                 SchemaDescriptors.enums(
-                        proto
-                            .getEnumType()
-                            .getValues()
-                            .stream()
-                            .map(EnumValueDescriptor::getName)
-                            .collect(Collectors.toList()))
+                        new ArrayList<>(enumValues.keySet()),
+                        new EnumValueAccessor<String>() {
+                          @Override
+                          public String createFrom(Object object) {
+                            return enumValues
+                                .keySet()
+                                .stream()
+                                .filter(e -> e.equals(object.toString()))
+                                .findFirst()
+                                .orElseThrow(
+                                    () ->
+                                        new IllegalArgumentException(
+                                            String.format(
+                                                "Unknown value [%s] for enum type. Values: %s",
+                                                object, enumValues.keySet())));
+                          }
+
+                          @Override
+                          public Object valueOf(String value) {
+                            return enumValues
+                                .values()
+                                .stream()
+                                .filter(v -> v.getName().equals(value))
+                                .findFirst()
+                                .orElseThrow(
+                                    () ->
+                                        new IllegalArgumentException(
+                                            String.format(
+                                                "Unknown value [%s] for enum type. Values: %s",
+                                                value, enumValues.keySet())));
+                          }
+                        })
                     .toTypeDescriptor();
         break;
       case MESSAGE:
         final String messageTypeName = proto.getMessageType().toProto().getName();
+        log.debug("Converting message {} with descriptor {}", messageTypeName, proto);
         structCache.computeIfAbsent(
-            messageTypeName, name -> convertProtoToSchema(proto.getMessageType()));
+            messageTypeName,
+            name ->
+                convertProtoToSchema(
+                    proto.getMessageType(),
+                    proto
+                        .getMessageType()
+                        .toProto()
+                        .getDefaultInstanceForType()));
         descriptor = (SchemaTypeDescriptor<T>) structCache.get(messageTypeName).toTypeDescriptor();
 
         break;
@@ -92,7 +184,25 @@ public class ProtoUtils {
     }
 
     if (proto.isRepeated()) {
-      return SchemaDescriptors.arrays(descriptor).toTypeDescriptor();
+
+      return SchemaDescriptors.arrays(
+              descriptor,
+              new ArrayValueAccessor<T>() {
+                @Override
+                public <V> List<T> values(V object) {
+                  @SuppressWarnings({"unchecked", "rawtypes"})
+                  List<T> cast = (List) object;
+                  return cast;
+                }
+
+                @Override
+                public T createFrom(Object object) {
+                  @SuppressWarnings({"unchecked", "rawtypes"})
+                  List<T> cast = (List) object;
+                  return (T) cast; // @TODO: WTF?
+                }
+              })
+          .toTypeDescriptor();
     } else {
       return descriptor;
     }
--- scheme/proto/src/test/java/cz/o2/proxima/scheme/proto/utils/ProtoUtilsTest.java
+++ scheme/proto/src/test/java/cz/o2/proxima/scheme/proto/utils/ProtoUtilsTest.java
@@ -15,21 +15,35 @@
  */
 package cz.o2.proxima.scheme.proto.utils;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import com.google.protobuf.ByteString;
+import cz.o2.proxima.scheme.AttributeValueAccessors.StructureValueAccessor;
 import cz.o2.proxima.scheme.AttributeValueType;
 import cz.o2.proxima.scheme.SchemaDescriptors.SchemaTypeDescriptor;
 import cz.o2.proxima.scheme.SchemaDescriptors.StructureTypeDescriptor;
 import cz.o2.proxima.scheme.proto.test.Scheme.Device;
 import cz.o2.proxima.scheme.proto.test.Scheme.ValueSchemeMessage;
+import cz.o2.proxima.scheme.proto.test.Scheme.ValueSchemeMessage.Directions;
+import cz.o2.proxima.scheme.proto.test.Scheme.ValueSchemeMessage.InnerMessage;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+@Slf4j
 public class ProtoUtilsTest {
 
   @Test
   public void testConvertSimpleProtoToSchema() {
-    SchemaTypeDescriptor<Device> schema = ProtoUtils.convertProtoToSchema(Device.getDescriptor());
+    SchemaTypeDescriptor<Device> schema =
+        ProtoUtils.convertProtoToSchema(Device.getDescriptor(), Device.getDefaultInstance());
     assertEquals(AttributeValueType.STRUCTURE, schema.getType());
     assertEquals(2, schema.getStructureTypeDescriptor().getFields().size());
     assertEquals(
@@ -44,13 +58,39 @@ public class ProtoUtilsTest {
             .getField("payload")
             .getArrayTypeDescriptor()
             .getValueType());
+
+    Device value =
+        Device.newBuilder()
+            .setType("test-type-value")
+            .setPayload(ByteString.copyFromUtf8("test-payload-value"))
+            .build();
+
+    StructureValueAccessor<Device> valueProvider =
+        schema.getStructureTypeDescriptor().getValueAccessor();
+    assertThrows(
+        IllegalArgumentException.class, () -> valueProvider.readField("unknown-field", value));
+    assertEquals("test-type-value", valueProvider.readField("type", value));
+    assertArrayEquals(
+        "test-payload-value".getBytes(StandardCharsets.UTF_8),
+        valueProvider.readFieldAsBytes("payload", value));
+    Map<String, Object> createFrom =
+        new HashMap<String, Object>() {
+          {
+            put("type", "test-type-value");
+            put("payload", "test-payload-value".getBytes());
+          }
+        };
+
+    Device created = valueProvider.createFrom(createFrom);
+    log.info("Proto object created from hashmap {}", created);
+    assertEquals(value, created);
   }
 
   @Test
-  @SuppressWarnings("unchecked")
   public void testConvertComplexProtoToSchema() {
     SchemaTypeDescriptor<ValueSchemeMessage> schema =
-        ProtoUtils.convertProtoToSchema(ValueSchemeMessage.getDescriptor());
+        ProtoUtils.convertProtoToSchema(
+            ValueSchemeMessage.getDescriptor(), ValueSchemeMessage.getDefaultInstance());
     assertEquals(AttributeValueType.STRUCTURE, schema.getType());
     StructureTypeDescriptor<ValueSchemeMessage> descriptor = schema.getStructureTypeDescriptor();
     assertEquals(ValueSchemeMessage.getDescriptor().getName(), descriptor.getName());
@@ -67,5 +107,72 @@ public class ProtoUtilsTest {
     assertEquals(
         AttributeValueType.STRUCTURE,
         descriptor.getField("repeated_inner_message").getArrayTypeDescriptor().getValueType());
+
+    final ValueSchemeMessage object =
+        ValueSchemeMessage.newBuilder()
+            .addRepeatedString("repeated_string_value_1")
+            .addRepeatedString("repeated_string_value_2")
+            .setInnerMessage(
+                InnerMessage.newBuilder()
+                    .addRepeatedInnerString("repeated_inner_string_value1")
+                    .addRepeatedInnerString("repeated_inner_string_value2")
+                    .setInnerDoubleType(38)
+                    .setInnerEnum(Directions.LEFT)
+                    .build())
+            .setStringType("string_type_value")
+            .setBooleanType(true)
+            .setLongType(100)
+            .setIntType(69)
+            .build();
+
+    final StructureValueAccessor<ValueSchemeMessage> valueProvider =
+        schema.getStructureTypeDescriptor().getValueAccessor();
+
+    assertEquals(
+        Arrays.asList("repeated_string_value_1", "repeated_string_value_2"),
+        valueProvider.readField("repeated_string", object));
+
+    Map<String, Object> innerMessage = valueProvider.readField("inner_message", object);
+
+    assertEquals(
+        Arrays.asList("repeated_inner_string_value1", "repeated_inner_string_value2"),
+        innerMessage.get("repeated_inner_string"));
+    assertEquals(38D, innerMessage.get("inner_double_type"));
+
+    assertEquals(Directions.LEFT.name(), innerMessage.get("inner_enum"));
+
+    assertEquals("string_type_value", valueProvider.readField("string_type", object));
+    assertEquals(true, valueProvider.readField("boolean_type", object));
+    assertEquals(
+        100L, Optional.ofNullable(valueProvider.readField("long_type", object)).orElse(-1));
+    assertEquals(69, Optional.ofNullable(valueProvider.readField("int_type", object)).orElse(-1));
+
+    final Map<String, Object> createFrom =
+        new HashMap<String, Object>() {
+          {
+            put(
+                "repeated_string",
+                Arrays.asList("repeated_string_value_1", "repeated_string_value_2"));
+            put(
+                "inner_message",
+                new HashMap<String, Object>() {
+                  {
+                    put(
+                        "repeated_inner_string",
+                        Arrays.asList(
+                            "repeated_inner_string_value1", "repeated_inner_string_value2"));
+                    put("inner_double_type", 38);
+                    put("inner_enum", Directions.LEFT.name());
+                  }
+                });
+            put("string_type", "string_type_value");
+            put("boolean_type", true);
+            put("long_type", 100);
+            put("int_type", 69);
+          }
+        };
+    ValueSchemeMessage created = valueProvider.createFrom(createFrom);
+    log.debug("New created message {}", created);
+    assertEquals(object, created);
   }
 }
